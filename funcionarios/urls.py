d7a28e929fa787be8f9a46b9557138cddf5bae84
DESKTOP-N7HHFNK\Matheus <matheuskbar@gmail.com>
2021-12-28 14:19:07 -0200

    Criando funcionario

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListarFuncionarios, EditarFuncionario, DeletarFuncionario
+from .views import ListarFuncionarios, EditarFuncionario, DeletarFuncionario, CriarFuncionario
 
 app_name = 'funcionarios'
 urlpatterns = [
     path('', ListarFuncionarios.as_view(), name='listar_funcionarios'),
+    path('novo/', CriarFuncionario.as_view(), name='criar_funcionario'),
     path('editar/<int:pk>', EditarFuncionario.as_view(), name='editar_funcionario'),
     path('deletar/<int:pk>', DeletarFuncionario.as_view(), name='deletar_funcionario'),
 ]
--- funcionarios/views.py
+++ funcionarios/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 
 from .models import Funcionario
 
@@ -11,6 +11,16 @@ class ListarFuncionarios(ListView):
         empresa_logada = self.request.user.funcionario.empresa
         queryset = Funcionario.objects.filter(empresa=empresa_logada)
         return queryset
+
+
+class CriarFuncionario(CreateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        nome_usuario = funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[1]
+        funcionario.empresa = self.request.user.funcionario.empresa
 
 
 class EditarFuncionario(UpdateView):
